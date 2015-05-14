9887935a29134606c87ade013d2dbb47ff6453b1
andrewbrandwood <andrew@brandwood.com>
2015-05-14 12:46:03 +0100

    removing socket.io references

--- public/_client/scripts/src/app.js
+++ public/_client/scripts/src/app.js
@@ -8,30 +8,7 @@
 		var _self = this;
 
 		function init(){
-			_self.socket = socket;
-			addListeners();
-
-			sendServerMessage();
-		}
-
-		function addListeners(){
-			_self.socket.on('server:basic-dispatch', showApp);
-			_self.socket.on('server:emit', showEveryone);
-		}
-
-		function showApp(val){
-			console.log(val);
 			
-		}
-
-		function showEveryone(val){
-			console.log(val);
-			//_self.socket.emit('client:basic-call', 0);
-		}
-
-		function sendServerMessage(){
-			//send a message to the server
-			_self.socket.emit('client:basic-call', 0);
 		}
 
 		init();
